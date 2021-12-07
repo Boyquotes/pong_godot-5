1ec8d03f68d0a0438f9197ea64a4d71856bd0d5f
dzdn <jdustinwilliams@gmail.com>
2021-12-07 10:56:58 -0500

    Complete episode 4.

--- pong-03/pong.gd
+++ pong-03/pong.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+# states
+enum GAME_STATE {MENU, SERVE, PLAY}
+var isPlayerServe = true
+
+# currentstate
+var currentGameState = GAME_STATE.MENU
+
 # screen values
 onready var screenWidth = get_tree().root.size.x
 onready var screenHeight = get_tree().root.size.y
@@ -36,16 +43,41 @@ var stringValue = "Hello World!"
 # string variable
 var stringPosition
 
+# delta key
+const RESET_DELTA_KEY = 0.0
+const MAX_KEY_TIME = 0.3
+var deltaKeyPress = 0.0
+
 func _ready() -> void:
 	print(get_tree().root.size)
 	font.font_data = robotoFile
 	font.size = fontSize
 	halfWidthFont = font.get_string_size(stringValue).x/2
 	heightFont = font.get_height()
-	stringPosition = Vector2(halfScreenWidth - halfWidthFont, heightFont)
 
 func _physics_process(delta: float):
-	pass
+	
+	deltaKeyPress += delta
+	
+	match currentGameState:
+		GAME_STATE.MENU:
+			changeString("MENU!!!")
+			if(Input.is_key_pressed(KEY_SPACE) and deltaKeyPress > MAX_KEY_TIME):
+				currentGameState = GAME_STATE.SERVE
+				deltaKeyPress = RESET_DELTA_KEY
+			update()
+		GAME_STATE.SERVE:
+			changeString("PLAY!!!")
+			if(Input.is_key_pressed(KEY_SPACE) and deltaKeyPress > MAX_KEY_TIME):
+				currentGameState = GAME_STATE.PLAY
+				deltaKeyPress = RESET_DELTA_KEY
+			update()
+		GAME_STATE.PLAY:
+			changeString("SERVE!!!")
+			if(Input.is_key_pressed(KEY_SPACE) and deltaKeyPress > MAX_KEY_TIME):
+				currentGameState = GAME_STATE.SERVE
+				deltaKeyPress = RESET_DELTA_KEY
+			update()
 
 func _draw() -> void:
 	setStartingPosition()
@@ -55,3 +87,9 @@ func setStartingPosition():
 	draw_rect(playerRectangle, paddleColor)
 	draw_rect(aiRectangle, paddleColor)
 	draw_string(font, stringPosition, stringValue)
+
+func changeString(newStringValue):
+	stringValue = newStringValue
+	halfWidthFont = font.get_string_size(stringValue).x/2
+	stringPosition = Vector2(halfScreenWidth - halfWidthFont, heightFont)
+	update()
